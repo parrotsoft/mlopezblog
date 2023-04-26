c7e268bd8dabded3ad940a2bbb1782229b9224a7
Ivan Andres L <ivan.lopez@evertecinc.com>
2023-04-25 22:19:00 -0500

    wip

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,10 +6,13 @@ use App\Domain\Post\PostDestroyAction;
 use App\Domain\Post\PostSaveAction;
 use App\Domain\Post\PostUpdateAction;
 use App\Http\Requests\PostRequest;
+use App\Models\Post;
+use App\Models\User;
 use App\ViewModels\PostViewModel;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\View\View;
 
 class PostController extends Controller
@@ -25,12 +28,25 @@ class PostController extends Controller
         return view('posts.create', new PostViewModel());
     }
 
-    public function store(PostRequest $request): RedirectResponse
+    public function store(Request $request): RedirectResponse
     {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|unique:posts|max:255',
+            'body' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('post/create')
+                ->withErrors($validator)
+                ->withInput();
+        }
+
         $request->request->add([
             'user_id' => Auth::user()->getAuthIdentifier()
         ]);
-        PostSaveAction::execute($request->all());
+
+        Post::query()->create($request->all());
+
         return redirect(route('posts.index'));
     }
 
