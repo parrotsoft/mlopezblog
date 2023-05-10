1b8984aea6704bc7320e4c09a02a242ea3199ca9
Miguel Lopez Ariza <Miguel.Lopez@evertecinc.com>
2023-05-10 17:57:23 -0500

    feat: add env paypal

--- config/services.php
+++ config/services.php
@@ -31,4 +31,10 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'paypal' => [
+        'clientId' => env('PAYPAL_CLIENT_ID'),
+        'secretKey' => env('PAYPAL_SECRET_KEY'),
+        'urlResource' => env('PAYPAL_URL_RESOURCE'),
+    ],
+
 ];
