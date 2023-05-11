98bc3eff425a13df399f5a7d1d4c3cb8a7a87b8e
Miguel Lopez Ariza <Miguel.Lopez@evertecinc.com>
2023-05-10 22:44:22 -0500

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -19,13 +19,46 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
+Artisan::command('pay', function () {
+    $result = Http::asForm()
+        ->withBasicAuth(config('services.paypal.clientId'), config('services.paypal.secretKey'))
+        ->post(config('services.paypal.urlResource') . 'oauth2/token', [
+            'grant_type' => 'client_credentials'
+        ]);
 
-Artisan::command('create', function () {
-    $config = config('services.paypal');
+    if ($result->ok()) {
+        $accessToken = $result->json()['access_token'];
 
-    $response = Http::post("{$config['urlResource']}v1/oauth2/token", [
-        "{$config['clientId']}:{$config['secretKey']}"
-    ]);
+        $order = Http::
+        withHeaders([
+            'Authorization' => 'Bearer ' . $accessToken,
+        ])->post(config('services.paypal.urlResource') . 'checkout/orders', [
+            'purchase_units' => [
+                [
+                    "reference_id" => uniqid(),
+                    'amount' => [
+                        'currency' => 'USD',
+                        'total' => '1.0'
+                    ],
+                ]
+            ],
+            'redirect_urls' => [
+                'return_url' => 'https://google.com',
+                'cancel_url' => 'https://google.com'
+            ]
+        ]);
 
-    dd($response->body());
+        if ($order->created()) {
+            $id = $order->json()['id'];
+            $links = collect($order->json()['links'])->where('method','=','REDIRECT')->first();
+            $link = $links['href'];
+            dd($link);
+        }
+
+    } else {
+        throw new Exception('Error de credenciales');
+    }
+
+
 });
+
